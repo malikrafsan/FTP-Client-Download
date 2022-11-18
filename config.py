cb7bcfeeb9bdc51fc9566074d83762938355aa39
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2022-11-19 05:47:15 +0700

    init

---
+++ config.py
@@ -0,0 +1,10 @@
+HOSTNAME = "167.205.34.55"
+USERNAME = "ftpuser"
+PASSWORD = "jarkomjaya"
+
+FILES = {
+  'upload/file-1Mb.txt': 'out/file-1Mb.txt',
+  'upload/file-2Mb.txt': 'out/file-2Mb.txt',
+  'upload/file-5Mb.txt': 'out/file-5Mb.txt'
+}
+
---
+++ main.py
@@ -0,0 +1,27 @@
+
+import ftplib
+import config
+from timeit import default_timer as timer
+
+def get_file(ftp_server: ftplib.FTP, file_server, file_client):
+  print("Downloading file: " + file_server)
+  start = timer()
+  
+  with open(file_client, "wb") as file:
+    ftp_server.retrbinary(f"RETR {file_server}", file.write)
+  
+  end = timer()
+  print(f"Downloaded in {end - start} seconds")
+
+def main():
+  ftp_server = ftplib.FTP(config.HOSTNAME, config.USERNAME, config.PASSWORD)
+  ftp_server.encoding = "utf-8"
+
+  files = config.FILES
+  for file in config.FILES:
+    get_file(ftp_server, file, files[file])
+
+  ftp_server.quit()
+
+if __name__ == '__main__':
+  main()
